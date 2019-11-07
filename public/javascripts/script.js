b7b47517ed9be3a8f504d2440f380b8a557e1702
hkevana <k.hendricks1015@gmail.com>
2019-11-07 04:22:32 +0000

    added players and stats

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,134 @@
+/* global Vue */
+/* global axios */
 var app = new Vue({
     el: '#app',
-    data: {},
-    methods: {},
+    data: {
+        error: false,
+        errorMessage: '',
+        displayResults: false,
+        inName: '',
+        inHeight: '',
+        inWeight: '',
+        players: [],
+        villians: [],
+    },
+    methods: {
+        async getTeam() {
+            console.log("called getTeam()");
+            const url = 'http://hkevana.com:4200/getTeam';
+            try {
+                const response = await axios.get(url);
+                console.log(response.data);
+                this.players = [];
+                if ( response.data.length > 0) {
+                    for(let i = 0; i < response.data.length; i++) {
+                        this.players.push({
+                            name: response.data[i].name,
+                            height: response.data[i].height,
+                            weight: response.data[i].weight,
+                            image: response.data[i].image,
+                        });
+                    }
+                    this.generateVillians();
+                    this.displayResults = true;
+                }
+            } catch(err) {
+                console.log(err);
+                this.displayResults = false;
+            }
+        },
+        async addPlayer() {
+            console.log("called addPlayer()");
+            const url = 'http://hkevana.com:4200/addPlayer';
+            const imgURL = 'http://hkevana.com:4200/getImage';
+            var img = '';
+            try {
+                const response = await axios.get(imgURL);
+                console.log(response.data);
+                img = response.data.url;
+            } catch(err) { console.log(err); }
+            console.log("image = ", img);
+            if (this.inName != '' && this.inHeight != '' && this.inWeight != '') {
+                if (this.players.length >= 5) {
+                    this.errorMessage = "roster can only contain 5 players";
+                    this.error = true;
+                    return;
+                }
+                axios.post(url, {
+                    name: this.inName,
+                    image: img,
+                    height: this.inHeight,
+                    weight: this.inWeight,
+                }).then(respoonse => {})
+                .catch(e => {
+                    console.log(e);
+                });
+                this.inName = '';
+                this.inHeight = '';
+                this.inWeight = '';
+                this.getTeam();
+            } else {
+                this.errorMessage = "Please provide input";
+                this.error = true;
+            }
+        },
+        clearAll() {
+            this.players = [];
+            const url = 'http://hkevana.com:4200/clear';
+            axios.delete(url);
+            this.displayResults = false;
+        },
+        async generateAllStars() {
+            console.log("called generateAllStars()");
+            const url = 'http://hkevana.com:4200/allstars';
+            try {
+                const response = await axios.get(url);
+                this.players = [];
+                for (let i = 0; i < response.data.length; i++) {
+                    this.players.push({
+                        name: response.data[i].name,
+                        height: response.data[i].height,
+                        weight: response.data[i].weight,
+                        image: response.data[i].image,
+                        position: response.data[i].position,
+                    });
+                }
+                this.generateVillians();
+                this.displayResults = true;
+                console.log(this.players);
+            } catch(err) { console.log(err) }
+        },
+        async generateVillians() {
+            console.log('called generateVillians()');
+            const url = "http://hkevana.com:4200/villians";
+            try {
+                const response = await axios.get(url);
+                this.villians = [];
+                for(let i = 0; i < response.data.length; i++) {
+                    this.villians.push({
+                        vName: response.data[i].name,
+                        vHeight: response.data[i].height,
+                        vWeight: response.data[i].weight,
+                        vImage: response.data[i].image,
+                        vPosition: response.data[i].position,
+                    });
+                }
+                console.log(this.villians);
+            } catch(err) { console.log(err) }
+            
+        },
+        clearError() {
+            this.error = false;
+            this.errorMessage = '';
+        },
+        playGame() {
+            
+        },
+        getRandNum(min, max) { 
+            return Math.floor((Math.random * max) + min); 
+        }
+    },
+    created: function() {
+        this.getTeam();
+    }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,130 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log('in main');
-  res.sendFile('index.html', { root: 'public'});
+    res.sendFile('index.html', { root: 'public'});
 });
+
+router.get('/getTeam', (req, res) => {
+    console.log('in getTeam');
+    console.log(players);
+    res.send(players);
+});
+
+router.get('/getImage', (req, res) => {
+    console.log('in getImage');
+    var access_token = 1007403959620690;
+    var id = getRandNum(0, 731);
+    console.log('id = ', id);
+    const url = 'https://superheroapi.com/api/' + access_token + '/' + id + '/image';
+    request(url).pipe(res);
+});
+
+router.get('/allstars', (req, res) => {
+    console.log('in allstars');
+    res.send(allstars);
+});
+
+router.get('/villians', (req, res) => {
+    console.log('in villians');
+    console.log(villianTeam);
+    res.send(villianTeam);
+    
+});
+
+router.post('/addPlayer', (req, res) => {
+    console.log('in addPlayer');
+    console.log(req.body);
+    players.push(req.body);
+    console.log(players);
+    console.log("length: ", players.length);
+    res.end('{"success": "Updated Successfully", "status": 200}');
+});
+
+router.delete('/clear', (req, res) => {
+    console.log('in clear');
+    players = [];
+    res.end('{"success": "Updated Successfully", "status": 200}');
+});
+
+var players = [];
+var allstars = [
+    {
+        name: 'Chuck Norris',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/954.jpg',
+        height: '178',
+        weight: '170',
+        position: 'point'
+    },
+    {
+        name: 'Batman',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/639.jpg',
+        height: '188',
+        weight: '210',
+        position: 'wing'
+    },
+    {
+        name: 'Kool-Aid Man',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/1343.jpg',
+        height: '183',
+        weight: '11000',
+        position: 'center'
+    },
+    {
+        name: 'Luke Skywalker',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/10447.jpg',
+        height: '170',
+        weight: '150',
+        position: 'shooting guard'
+    },
+    {
+        name: 'Ben 10',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/1365.jpg',
+        height: '165',
+        weight: '125',
+        position: 'small forward'
+    }
+];
+
+var villianTeam = [
+    {
+        name: 'Darth Vader',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/10444.jpg',
+        height: '190',
+        weight: '196',
+        position: 'shooting guard'
+    },
+    {
+        name: 'Chuck Norris',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/954.jpg',
+        height: '178',
+        weight: '170',
+        position: 'point'
+    },
+    {
+        name: 'Deadpool',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/835.jpg',
+        height: '188',
+        weight: '210',
+        position: 'small forward'
+    },
+    {
+        name: 'Venom',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/22.jpg',
+        height: '193',
+        weight: '260',
+        position: 'wing'
+    },
+    {
+        name: 'King Kong',
+        image: 'https://www.superherodb.com/pictures2/portraits/10/100/10591.jpg',
+        height: '762',
+        weight: '800',
+        position: 'center'
+    }
+];
+function getRandNum(min, max) { return Math.floor((Math.random() * max) + min); }
 
 module.exports = router;
